bd4524eb8eb1b8924046dd8669f3190d8dd4d643
cameronpool <cameronpool2019@gmail.com>
2024-08-17 15:23:31 -0500

    about done

--- digital-business-card/src/components/About.jsx
+++ digital-business-card/src/components/About.jsx
@@ -1,5 +1,12 @@
+import "./About.css";
+
 function About() {
-    return <h1>about</h1>
+  return (
+    <div className="about-container">
+      <h1>About Me</h1>
+      <h3>Skilled full stack developer</h3>
+    </div>
+  );
 }
 
 export default About;
