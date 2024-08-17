6b8620c5a97097f2c33edea7e87f2e73d2910a92
cameronpool <cameronpool2019@gmail.com>
2024-08-17 15:18:58 -0500

    links section completed

--- digital-business-card/src/components/Hero.jsx
+++ digital-business-card/src/components/Hero.jsx
@@ -1,13 +1,13 @@
-import headshot from '../assets/Headshot.jpg';
-import './Hero.css';
+import headshot from "../assets/Headshot.jpg";
+import "./Hero.css";
 
 function Hero() {
-    return (
-        <div className='hero'>
-            <img className='hero-img' src={headshot} alt="picture of me" />
-            <h2>Software Engineer</h2>
-        </div>
-    )
+  return (
+    <div className="hero">
+      <img className="hero-img" src={headshot} alt="picture of me" />
+      <h2>Software Engineer</h2>
+    </div>
+  );
 }
 
 export default Hero;
--- digital-business-card/src/components/Links.jsx
+++ digital-business-card/src/components/Links.jsx
@@ -1,5 +1,21 @@
+import "./Link.css";
+
 function Links() {
-    return <h1>links</h1>
+  return (
+    <div className="link-container">
+      <h3 className="link">
+        <a href="https://www.linkedin.com/in/cameron-pool-9b1a6b313/">
+          LinkedIn
+        </a>
+      </h3>
+      <h3 className="link">
+        <a href="https://github.com/CPoooo">GitHub</a>
+      </h3>
+      <h3 className="link">
+        <a href="https://cameronpool.netlify.app">Portfolio</a>
+      </h3>
+    </div>
+  );
 }
 
 export default Links;
